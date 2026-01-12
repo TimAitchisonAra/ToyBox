8200c53757dccd7de50a5699c8f5450a3b7e7d8c
TimAitchison <tim.aitchison@ara.ac.nz>
2026-01-13 11:39:47 +1300

    nasty force push!

--- ToyBoxTests/TestBox.cs
+++ ToyBoxTests/TestBox.cs
@@ -35,5 +35,18 @@ namespace ToyBoxTests
 
             Assert.That(expected, Is.EqualTo(actual));
         }
+
+        [Test]
+        public void ToyBox_ToString_ProducesCorrectString()
+        {
+            theBox.AddToy("Teddy", "brown", 12.34M);
+            theBox.AddToy("Doll", "pink", 34.56M);
+
+            string expected = "A box containing 2 toys\na brown colored Teddy worth $12.34\na pink colored Doll worth $34.56";
+            string actual = theBox.ToString();
+            Console.Write(actual);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
     }
 }
